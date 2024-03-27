4a2e1b5624d7d1d58ca3499eef4da15ea075e6d1
ramon chuinca <ramonchuinca@hotmail.com>
2024-03-26 23:05:33 -0400

    subindo style da tabela de listagem php

--- listarConsultas.php
+++ listarConsultas.php
@@ -18,23 +18,47 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Exibir os dados encontrados em uma tabela
-    echo "<table border='5'>";
+    echo "<table border='15'>";
     echo "<tr> <th>Nome</th> <th>Email</th> <th>Telefone</th> <th>CPF</th> <th>Endereço</th> <th>Data</th> </tr>";
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>". $row["nome"] ."</td>";
-        echo "<td>". $row["email"] ."</td>";
-        echo "<td>". $row["telefone"] ."</td>";
-        echo "<td>". $row["cpf"] ."</td>";
-        echo "<td>". $row["endereco"] ."</td>";
-        echo "<td>". $row["data"] ."</td>";
-        echo "</tr>";
+    echo "<style>
+    table {
+        width: 100%;
+        
+        border: 2px solid red;
+        font-family: Arial, sans-serif;
+        border-radius: 8px;
     }
-    echo "</table>";
+    th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 2px solid #ddd;
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+    tr:hover {
+        background-color: #ddd;
+    }
+</style>";
+  while($row = $result->fetch_assoc()) {
+
+       echo "<tr>";
+       echo "<td>". $row["nome"] ."</td>";
+       echo "<td>". $row["email"] ."</td>";
+       echo "<td>". $row["telefone"] ."</td>";
+       echo "<td>". $row["cpf"] ."</td>";
+       echo "<td>". $row["endereco"] ."</td>";
+       echo "<td>". $row["data"] ."</td>";
+       echo "</tr>";
+   }
+ echo "</table>";
 } else {
     echo "0 resultados encontrados";
 }
 
-// Fechar a conexão
+ //Fechar a conexão
 $conn->close();
 ?>;
