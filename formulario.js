5b063583020ec3e8e9b20616b5e67ec4e0854015
ramonchuinca <ramonchuinca@hotmail.com>
2024-06-28 12:03:46 -0400

    atualizando

--- formulario.js
+++ formulario.js
@@ -9,3 +9,33 @@ document.addEventListener('DOMContentLoaded', function () {
         this.classList.remove('hovered');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let elemento = document.getElementById('Remedios');
+
+    // Função para adicionar a classe quando o toque começa
+    function addHoverClass() {
+        elemento.classList.add('hovered');
+    }
+
+    // Função para remover a classe quando o toque termina
+    function removeHoverClass() {
+        elemento.classList.remove('hovered');
+    }
+
+    // Ouvinte de evento para touchstart (quando o toque começa)
+    elemento.addEventListener('touchstart', function () {
+        addHoverClass();
+    });
+
+    // Ouvinte de evento para touchend (quando o toque termina)
+    elemento.addEventListener('touchend', function () {
+        removeHoverClass();
+    });
+
+    // Ouvinte de evento para touchcancel (quando o toque é cancelado, por exemplo, se o dedo sair da área do elemento)
+    elemento.addEventListener('touchcancel', function () {
+        removeHoverClass();
+    });
+});
+
